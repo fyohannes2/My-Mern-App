8736b995374d089d33bd03e12a6a02c78bef67d2
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-23 15:39:19 -0400

    heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 const port = process.env.PORT || 5000;
+const mongoose =  require("mongoose");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,12 +13,54 @@ app.use(cors());
 mongoose.connect("mongodb+srv://admin:abc1234@cluster1.xcrkx.mongodb.net/?retryWrites=true&w=majority")
 
 
+//data schema and model
+const movieSchema = {
+    title: String,
+    genre: String,
+    year: String
+}
+
+const Movie = mongoose.model("Movie", movieSchema);
+
 //API routes
 app.get('/movies', function(req, res) {
-    re.send("express is here")
+    Movie.find().then(movies => res.json(movies));
 })
 
+//add movie
+app.post('/newmovie', function(req, res) {
+    const title = req.body.title;
+    const genre = req.body.genre;
+    const year = req.body.year;
+
+    const newMovie = new Movie({
+        title,
+        genre,
+        year
+    });
+
+    newMovie.save();
+
+});
+
+app.delete('/delete/:id', function(req, res) {
+    const id = req.params.id;
+    Movie.findByIdAndDelete({_id: id}, function(err) {
+        if(!err) {
+            console.log("movie deleted");
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(port, function() {
     console.log("express is running");
-}) 
+})
